1c20be5516fd29203e314119951f15a0f5be86bf
Joe Sandmeyer <github.com@unconscious.com>
2023-10-12 11:44:00 -0400

    Add experience provider, about page, home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-//import { useState } from 'react'
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ExperienceProvider } from '@vatom/experience-sdk';
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
+//
 function App() {
-  //const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter basename={"/3UewHF0ge9" }>
+      <ExperienceProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/about" element={<AboutPage />}></Route>
+        </Routes>
+      </ExperienceProvider>
+    </BrowserRouter>
   )
 }
-
-export default App
+//
+export default App;
---
+++ src/pages/About.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import './Common.css'
+//
+const AboutPage = () => {
+  return (
+		<div>
+			<h1>Welcome to the About page</h1>
+			<p>version 0-0-001</p>
+			<br/>
+			<Link to="/home"> Return to the Home page </Link>
+		</div>
+  )
+}
+//
+export default AboutPage;
