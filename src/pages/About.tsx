9ec6d94fcbe5f3a4740d5927d9a95c2b2ef5a89b
Joe Sandmeyer <github.com@unconscious.com>
2023-10-12 12:10:59 -0400

    tick version num

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ const AboutPage = () => {
   return (
 		<div>
 			<h1>Welcome to the About page</h1>
-			<p>version 0-0-001</p>
+			<p>version 0-0-002</p>
 			<br/>
 			<Link to="/home"> Return to the Home page </Link>
 		</div>
