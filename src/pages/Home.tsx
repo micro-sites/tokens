eaa87be38cd4327c106f6a370fa38900a378e41a
Joe Sandmeyer <github.com@unconscious.com>
2023-10-12 12:09:18 -0400

    check token inventory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import { useBusiness } from "@vatom/experience-sdk";
 import './Common.css'
 //
 const HomePage = () => {
+	const { tokens } = useBusiness();
+
+	React.useEffect(() => {
+		console.log('NFTs at Home page: ', tokens);
+	}, []);	
+
   return (
 		<div>
 			<h1>Welcome to the Home page</h1>
