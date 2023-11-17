d372277171c108a98a66457639ba35b6db8d639e
Joe Sandmeyer <github.com@unconscious.com>
2023-11-16 19:00:38 -0500

    v 0-0-005 -- log exp kit features

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import './Common.css'
+import { useExperience, useBusiness, useUser } from "@vatom/experience-sdk";
 //
 const AboutPage = () => {
+	const allUseExperience = useExperience();
+	const allUseBusiness = useBusiness();
+	const allUseUser = useUser();
+
+  console.log('allUseExperience :', allUseExperience);
+  console.log('allUseBusiness :', allUseBusiness);
+  console.log('allUseUser :', allUseUser);
+
   return (
 		<div>
 			<h1>Welcome to the About page</h1>
